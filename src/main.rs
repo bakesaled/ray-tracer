208f8c940cdc545f6e56ca57b4fe266ab4fb7156
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2022-08-22 17:35:32 -0600

    Output an image

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remaining: {}", j);
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH as f64 - 1_f64);
+            let g = j as f64 / (IMAGE_HEIGHT as f64 - 1_f64);
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+
+    eprintln!("Done.");
+}
