a98c3952b8b73144406a4bb2e84ed67c76424622
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2022-08-28 15:21:30 -0600

    initial ray tracer

--- src/lib.rs
+++ src/lib.rs
@@ -170,3 +170,29 @@ pub fn write_color(pixel_color: Color) {
         (255.999 * pixel_color.z()) as i32
     );
 }
+
+pub struct Ray {
+    orig: Point3,
+    dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: &Point3, direction: &Vec3) -> Ray {
+        Ray {
+            orig: *origin,
+            dir: *direction,
+        }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.orig
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        &self.orig + &(t * &self.dir)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-use ray_tracer;
+use ray_tracer::{self, unit_vector, write_color, Color, Point3, Ray, Vec3};
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    &((1.0 - t) * &Color::new(1.0, 1.0, 1.0)) + &(t * &Color::new(0.5, 0.7, 1.0))
+}
 
 fn main() {
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+    // Image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
 
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = &((&origin - &horizontal) / 2_f64)
+        - &(&(vertical / 2_f64) - &Vec3::new(0.0, 0.0, focal_length));
+
+    // Render
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
     println!("255");
@@ -11,12 +31,14 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = ray_tracer::Color::new(
-                i as f64 / (IMAGE_WIDTH as f64 - 1_f64),
-                j as f64 / (IMAGE_HEIGHT as f64 - 1_f64),
-                0.25,
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let r = Ray::new(
+                &origin,
+                &(&(&((&lower_left_corner) + &(u * &horizontal)) + &(v * &vertical)) - &origin),
             );
-            ray_tracer::write_color(pixel_color);
+            let pixel_color = ray_color(&r);
+            write_color(pixel_color);
         }
     }
 
