9c239c957831520802f94685a1f4157d0580fbbf
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2022-09-21 15:36:20 -0600

    fuzzy metal

--- src/ray_tracer/metal.rs
+++ src/ray_tracer/metal.rs
@@ -1,19 +1,30 @@
-use crate::{Color, Material, Ray};
+use crate::{random_in_unit_sphere, Color, Material, Ray};
 
 pub struct Metal {
     pub albedo: Color,
+    pub fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(color: Color) -> Metal {
-        Metal { albedo: color }
+    pub fn new(color: Color, fuzz: f64) -> Metal {
+        let mut f = 1.0;
+        if fuzz < 1.0 {
+            f = fuzz;
+        }
+        Metal {
+            albedo: color,
+            fuzz: f,
+        }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, ray: crate::Ray, rec: crate::HitRecord) -> crate::MaterialRecord {
         let reflected = ray.direction().unit_vector().reflect(rec.normal.unwrap());
-        let scattered = Ray::new(rec.p.unwrap(), reflected);
+        let scattered = Ray::new(
+            rec.p.unwrap(),
+            reflected + self.fuzz * random_in_unit_sphere(),
+        );
 
         return crate::MaterialRecord {
             attenuation: self.albedo,
